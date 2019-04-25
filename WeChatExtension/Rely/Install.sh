c53f6e970c372f7841e54f4641ac95d5b608a7ba
MustangYM <ymoomy@yeah.net>
2019-04-25 15:52:04 +0800

    资源

--- WeChatExtension/Rely/Install.sh
+++ WeChatExtension/Rely/Install.sh
@@ -14,7 +14,7 @@ fi
 
 app_name="WeChat"
 shell_path="$(dirname "$0")"
-framework_name="WeChatPlugin"
+framework_name="WeChatExtension"
 app_bundle_path="${wechat_path}/Contents/MacOS"
 app_executable_path="${app_bundle_path}/${app_name}"
 app_executable_backup_path="${app_executable_path}_backup"
@@ -39,7 +39,7 @@ read -t 150 -p "已安装微信小助手，是否覆盖？[y/n]:" result
 fi
 
 if [[ "$result" == 'y' ]]; then
-    cp -r "${shell_path}/Products/Debug/${framework_name}.framework" ${app_bundle_path}
+    cp -r "${shell_path}/Plugin/${framework_name}.framework" ${app_bundle_path}
     ${shell_path}/insert_dylib --all-yes "${framework_path}/${framework_name}" "$app_executable_backup_path" "$app_executable_path"
     ${shell_path}/UpdateRemoteControlCommandsPlist.py
 fi
