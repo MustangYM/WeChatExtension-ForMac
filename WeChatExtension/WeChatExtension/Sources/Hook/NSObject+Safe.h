d2b9c2119ab5751fd7defa9ef3d7c325b7a8c230
yanmao <yanmao@yy.com>
2020-04-26 13:15:53 +0800

    崩溃拦截，适配笔记皮肤，茱萸粉，适配群语音或视频皮肤

---
+++ WeChatExtension/WeChatExtension/Sources/Hook/NSObject+Safe.h
@@ -0,0 +1,20 @@
+//
+//  NSObject+Safe.h
+//  WeChatExtension
+//
+//  Created by MustangYM on 2020/4/26.
+//  Copyright © 2020 MustangYM. All rights reserved.
+//
+
+#import <AppKit/AppKit.h>
+
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NSObject (Safe)
+
+@end
+
+NS_ASSUME_NONNULL_END
